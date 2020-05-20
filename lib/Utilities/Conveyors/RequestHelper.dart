f7122a865c7690561354caabee44acabf09bbbf1
heshanka <info@heshanka.com>
2020-05-20 18:01:39 +0300

    updated RequestHelper to include uploadHeaders

--- lib/Utilities/Conveyors/RequestHelper.dart
+++ lib/Utilities/Conveyors/RequestHelper.dart
@@ -5,10 +5,17 @@ import '../Preference.dart';
 class RequestHelper {
 
   static const Map<String, String> postHeaders = {"Accept": "application/json", "Content-Type": "application/json"};
+  static const Map<String, String> uploadHeaders = {"Content-Type": "multipart/form-data"};
 
   static Future<Map<String, String>> getPostHeaders() async {
     Map<String, String> headers = await getAuthHeader();
     headers.addAll(postHeaders);
+    return headers;
+  }
+
+    static Future<Map<String, String>> getUploadHeaders() async {
+    Map<String, String> headers = await getAuthHeader();
+    headers.addAll(uploadHeaders);
     return headers;
   }
 
